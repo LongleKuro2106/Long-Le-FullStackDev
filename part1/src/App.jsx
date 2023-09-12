1fcd054a5f7947b9ce1fd4c93cee81533882b891
Long Le <long.le21062004@gmail.com>
2023-09-12 23:04:47 +0300

    Exercises

---
+++ part1/src/App.jsx
@@ -0,0 +1,55 @@
+const App = () => {
+  // Define the parts array
+  const parts = [
+    { name: 'Fundamentals of React', exercises: 10 },
+    { name: 'Using props to pass data', exercises: 7 },
+    { name: 'State of a component', exercises: 14 },
+  ];
+
+  return (
+    <div>
+      <Header course="Half Stack application development" />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  );
+};
+
+
+const Header = ({ course }) => {
+  return (
+    <div>
+      <h1>{course}</h1>
+    </div>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part, index) => (
+        <Part key={index} part={part} />
+      ))}
+    </div>
+  );
+};
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+};
+
+
+
+const Total = ({ exercises1, exercises2, exercises3 }) => {
+  return (
+    <div>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div>
+  )
+}
+
+export default App;
